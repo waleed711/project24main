cf00c986ee07965862f8acb34011d42928e03440
waleed711 <85139457+waleed711@users.noreply.github.com>
2021-08-04 10:36:10 -0500

    Add files via upload

---
+++ Project_Template_24-main/js/playerArcher.js
@@ -0,0 +1,39 @@
+class playerArcher {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: true
+      };
+  
+      this.body = Bodies.rectangle(x, y, width, height, options);
+  
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("./assets/playerArcher.png");
+  
+      World.add(world, this.body);
+    }
+  
+   
+    display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+      Matter.Body.setAngle(this.body,-PI/2);
+      if (move === "UP" && playerArcher.body.angle < 1.77){
+        angleValue = 0.1;
+    }else{
+        angleValue = -0.1;
+    }
+    if (move === "DOWN" && playerArcher.body.angle < 1.47){
+        angleValue = -0.1;
+    }else{
+        angleValue = 0.1
+    }
+    }
+  }
+  
